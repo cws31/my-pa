b90e1de14017f39050c4110d5353a8ab0d63acc7
sonu kumar <gautamrocky909621@gmail.com>
2026-08-26 16:48:56 +0530

    feat(user): add User entity and repository
    created user's blue print and repo to persist the object of blueprint in db

---
+++ src/main/java/cs/sonu/personalAssiatnt/model/User.java
@@ -0,0 +1,22 @@
+package cs.sonu.personalAssiatnt.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; // Will be BCrypt encoded
+}
