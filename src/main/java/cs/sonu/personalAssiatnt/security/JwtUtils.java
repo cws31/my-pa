96fec2b65bf8cd0ee6ac101cad7647d1a69aa354
sonu kumar <gautamrocky909621@gmail.com>
2026-08-26 16:53:31 +0530

    feat(security): add JWT utilities and authentication filter
    implemented security layer

---
+++ src/main/java/cs/sonu/personalAssiatnt/security/JwtUtils.java
@@ -0,0 +1,38 @@
+package cs.sonu.personalAssiatnt.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private final Key jwtSecret = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long jwtExpirationMs = 86400000; // 24 Hours
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(jwtSecret)
+                .compact();
+    }
+
+    String getUsernameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(jwtSecret).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(authToken);
+            return true;
+        } catch (JwtException e) {
+            System.err.println("Invalid JWT token: " + e.getMessage());
+        }
+        return false;
+    }
+}
